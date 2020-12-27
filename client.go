4161891d5ec74042029392faee9e54403f44884f
LAPTOP-3E4II3DG\maotan <mt_126@126.com>
2020-12-27 18:51:21 +0800

    http请求

--- client.go
+++ client.go
@@ -5,9 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/maotan/go-truffle/cloud"
 	"github.com/maotan/go-truffle/cloud/serviceregistry"
+	"github.com/maotan/go-truffle/truffle"
 	"github.com/maotan/go-truffle/util"
-	"log"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
@@ -29,25 +30,34 @@ func main() {
 	fmt.Println(ip)
 	rand.Seed(time.Now().UnixNano())
 
-	si, _ := cloud.NewDefaultServiceInstance("go-client-server", ip, 6000,
+	si, _ := cloud.NewDefaultServiceInstance("go-client-server", ip, 9000,
 		false, map[string]string{"client": "zyn3"}, "")
 
 	registryDiscoveryClient.Register(si)
 
 	r := gin.Default()
 	r.GET("/actuator/health", func(c *gin.Context) {
-		log.Print("1111111")
-		log.Print( registryDiscoveryClient.GetServices())
-		log.Print("222222")
-		log.Print(registryDiscoveryClient.GetInstances("consul"))
-		log.Print(registryDiscoveryClient.GetInstances("go-user-server"))
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	err = r.Run(":6000")
-	if err != nil {
-		registryDiscoveryClient.Deregister()
-	}
+	r.GET("/client/ping", func(c *gin.Context) {
+		instances, err  := registryDiscoveryClient.GetInstances("go-user-server")
+		if err != nil{
+			panic(truffle.NewWarnError(600 ,"找不到server"))
+		}
+		instance := instances[0]
+		url := fmt.Sprintf("http://%s:%d/%s",instance.GetHost(), instance.GetPort(), "v2/ping")
+
+		fmt.Println("url:", url)
+		resp, err := http.Get (url)
+		if err!=nil{
+
+		}
+		dd, _:= truffle.ParseResponse(resp)
+		c.JSON(resp.StatusCode, dd)
+	})
+	r.Use(truffle.Recover)
+	r.Run(":9001")
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/maotan/go-truffle v0.0.0-20201221140404-a9ca436b54da
+	github.com/maotan/go-truffle v1.1.1
 )
